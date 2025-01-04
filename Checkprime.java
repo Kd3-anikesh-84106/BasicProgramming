22141ccfc63750a7a8fe720a5aa53d00848362da
Kd3-anikesh-84106 <anikeshghosh94@gmail.com>
2025-01-04 12:25:31 +0530

    Check for prime No

---
+++ Checkprime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Checkprime {
+
+    public static void main(String args[]){
+       
+        int N;
+
+        System.out.println("Enter a Number to check for Prime");
+
+        Scanner sc = new Scanner(System.in);
+
+        N= sc.nextInt();
+
+        System.out.println("Entered No. is :"+N);
+
+        int count=0;
+        for(int i=1;i<=N;i++)
+        {
+            if(N%i==0)
+            count++;
+        }
+        if(count==2)
+        System.out.println("The input No. is prime No");
+        else
+        System.out.println("It is not a prime No.");
+    }
+    
+}
